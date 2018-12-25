1ca854883aa31ccd5f681be3ecb4c4aad17f2cc0
akudryakov <akudryakov@users.noreply.github.com>
2018-12-25 23:24:58 +0300

    Добавлены тесты для классов Point и Square

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point(4,0);
+    Point p2 = new Point(0,2);
+    Assert.assertEquals(p1.distance(p2.x,p2.y),4.47213595499958);
+  }
+
+  @Test
+  public void testZeroDistance() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
+    Assert.assertEquals(p1.distance(p2.x, p2.y), 0);
+  }
+
+  @Test
+  public void testMinusDistance() {
+    Point p1 = new Point(-1, -1);
+    Point p2 = new Point(1, -1);
+    Assert.assertEquals(p1.distance(p2.x, p2.y), 2);
+  }
+
+
+}

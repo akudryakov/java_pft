525672107697d5fa64657f91fce3cf52291ec8a9
akudryakov <akudryakov@users.noreply.github.com>
2018-12-25 03:36:34 +0300

    Задание 2.
    Создание классов Point и PointDistance

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+public class PointDistance {
+  public static void main(String[] args) {
+    //реализация с использованием функции distance класса PointDistance
+    Point p1 = new Point(4, -3);
+    Point p2 = new Point(1, 0);
+    System.out.println("Distance between point1 " + " x =" + p1.x + " , " + "y =" + p1.y + " and point2" + " x =" + p2.x + " , " + "y =" + p2.y + " = " + distance(p1, p2));
+
+    //реализация с вызовом метода класса Point
+    Point dis = new Point(4,-3,1,0);
+    System.out.println("Distance between point1 " + " x =" + p1.x + " , " + "y =" + p1.y + " and point2" + " x =" + p2.x + " , " + "y =" + p2.y + " = " + dis.distance());
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)));
+  }
+
+}

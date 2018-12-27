404858457083f2c5b74cf7b9d6d69d06ce196568
Aleksandr Kudryakov <akudryakov@cinimex.ru>
2018-12-27 16:36:29 +0300

    Исправил Тесты для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,21 +9,22 @@ public class PointTests {
   public void testDistance(){
     Point p1 = new Point(4,0);
     Point p2 = new Point(0,2);
-    Assert.assertEquals(p1.distance(p2.x,p2.y),4.47213595499958);
+
+    Assert.assertEquals(p1.distance(p2),4.47213595499958);
   }
 
   @Test
   public void testZeroDistance() {
     Point p1 = new Point(0, 0);
     Point p2 = new Point(0, 0);
-    Assert.assertEquals(p1.distance(p2.x, p2.y), 0);
+    Assert.assertEquals(p1.distance(p2), 0);
   }
 
   @Test
   public void testMinusDistance() {
     Point p1 = new Point(-1, -1);
     Point p2 = new Point(1, -1);
-    Assert.assertEquals(p1.distance(p2.x, p2.y), 2);
+    Assert.assertEquals(p1.distance(p2), 2);
   }
 
 

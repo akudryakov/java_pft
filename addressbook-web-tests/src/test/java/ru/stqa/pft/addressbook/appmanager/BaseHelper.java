400a878342013d2bf9e318c1a73ab4d70a3c7e97
akudryakov <akudryakov@users.noreply.github.com>
2019-02-05 01:02:37 +0300

    Лекция 3.6. Оптимизация заполнения форм: оставляем не изменяющися значения в полях

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -18,26 +18,12 @@ public class BaseHelper {
   }
 
   protected void type(By locator, String text) {
-    wd.findElement(locator).clear();
-    wd.findElement(locator).sendKeys(text);
-  }
-
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! text.equals(existingText)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
     }
   }
-
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
 }

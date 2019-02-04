ed66745f4075e1b29c33576e90d37e6a3758f3ea
akudryakov <akudryakov@users.noreply.github.com>
2019-02-04 18:39:51 +0300

    Лекция 3.1 Проверка условий при помощи конструкции if-then-else

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -9,7 +9,23 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testUserCreation() throws Exception {
     app.getContactHelper().addNewUser();
-    app.getContactHelper().fillUsersInfo(new ContactData("Test Name", "Test Middle Name", "Test Last Name", "Test Nickname", "me", "Test Corp.", "Test Address", "1", "2", "3", "4", "test@testmail.test", "test2@testmail.test", "test3@testmail.test"));
+    app.getContactHelper().fillUsersInfo(new ContactData(
+            "Test Name",
+            "Test Middle Name",
+            "Test Last Name",
+            "Test Nickname",
+            "me",
+            "Test Corp.",
+            "Test Address",
+            "1",
+            "2",
+            "3",
+            "4",
+            "test@testmail.test",
+            "test2@testmail.test",
+            "test3@testmail.test"
+            )
+    );
     app.getContactHelper().submit();
     app.getNavigationHelper().goToHomePage();
   }

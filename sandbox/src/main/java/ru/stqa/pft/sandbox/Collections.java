bc044e1252fc90d9bf77f63c1e9f65c6bd1f28c7
akudryakov <akudryakov@users.noreply.github.com>
2019-03-11 02:32:06 +0300

    4.2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
   public static void main(String[] args) {
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (int i = 0; i < langs.length; i++) {
-      System.out.println("Я хочу выучить " + langs[i]);
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
     }
+
   }
 }

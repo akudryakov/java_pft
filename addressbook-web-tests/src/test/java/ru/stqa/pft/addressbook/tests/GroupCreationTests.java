359c0d37c16cddb7ae8c0584d5cb97195c672691
akudryakov <akudryakov@users.noreply.github.com>
2019-03-13 23:50:03 +0300

    4.9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -14,28 +14,24 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1",null,null);
+        GroupData group = new GroupData("test3",null,null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;  //получаем максимальынй id сравнением в цикле
+
+        /*
+         int max = 0;  //получаем максимальынй id сравнением в цикле
         for (GroupData g : after) {
             if (g.getId() > max) {
                 max = g.getId();
             }
         }
-
-        Comparator<? super GroupData> byId = new Comparator<GroupData>() { //получаем максимальный id с помощью компоратора
-            @Override
-            public int compare(GroupData o1, GroupData o2) {
-                return Integer.compare(o1.getId(), o2.getId());
-            }
-        };
-        int max1 = after.stream().max(byId).get().getId();
-        group.setId(max1);
+        */
 
 
+        //получаем максимальный id с помощью компоратора лямбда функция.
+        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group); //предсказываем рузельтат
         Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
     }
